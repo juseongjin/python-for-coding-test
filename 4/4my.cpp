43b6ad60ca82d6f64361a0efd059b3460b26024d
seongjinju <cyse3062@naver.com>
2022-12-29 17:36:48 +0900

    training

--- 4/4my.cpp
+++ 4/4my.cpp
@@ -2,56 +2,56 @@
 
 using namespace std;
 
+int d[50][50];
+int arr[50][50];
 int n, m, x, y, pose;
+
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, -1, 0, 1};
+
+void turn(){
+    pose -=1;
+    if(pose==-1) pose=3;
+}
 
 int main(){
     cin >> n >> m;
     cin >> x >> y >> pose;
-    int map[n][m]={0,};
-    int result=1;
+    d[x][y] = 1;
+    int result=1, turn_time=0;
+
     //지도 만들기
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            cin >> map[i][j];
+            int x;
+            cin >> x;
+            arr[i][j] = x;
         }
     }
     
     while(true){
-        if(map[x][y-1]==0 || map[x][y+1]==0 || map[x-1][y]==0 || map[x+1][y]==0){
-            if(pose==0){
-                if(map[x][y-1]==0){
-                    map[x][y]=1;
-                    result+=1;
-                    y-=1;
-                }
-                pose=3;
-            }
-            else if(pose==3){
-                if(map[x+1][y]==0){
-                    map[x][y]=1;
-                    result+=1;
-                    x+=1;
-                }
-                pose=2;
-            }
-            else if(pose==2){
-                if(map[x][y+1]==0){
-                    map[x][y]=1;
-                    result+=1;
-                    y+=1;
-                }
-                pose=1;
-            }
-            else if(pose==1){
-                if(map[x-1][y]==0){
-                    map[x][y]=1;
-                    result+=1;
-                    x-=1;
-                }
-                pose=0;
-            }
+        turn();
+        int nx = x + dx[pose];
+        int ny = y + dy[pose];
+        if(d[nx][ny]==0 && arr[nx][ny]==0){
+            d[nx][ny]=1;
+            x = nx;
+            y = ny;
+            result+=1;
+            turn_time = 0;
+            continue;
         }
-        else break;
+        else turn_time+=1;
+        if(turn_time==4){
+            nx = x - dx[pose];
+            ny = y - dy[pose];
+            if(arr[nx][ny]==0){
+                x = nx;
+                y = ny;
+            }
+            else break;
+            turn_time = 0;
+        }
     }
     cout << result << endl;
     return 0;
